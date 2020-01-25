0ce50616dc25224dd5974f1356551531528f0998
Michael Bykovski <michael.bykovskii@aoe.com>
2020-01-25 16:24:03 +0100

    Add all tests for mapper

--- astParser/mapper_test.go
+++ astParser/mapper_test.go
@@ -52,10 +52,129 @@ func Test_selectorExprToField(t *testing.T) {
 	)
 }
 
-func Test_arrayTypeToField(t *testing.T) {}
-func Test_ellipsisToField(t *testing.T) {}
-func Test_interfaceTypeToField(t *testing.T) {}
-func Test_mapTypeToField(t *testing.T) {}
-func Test_funcTypeToField(t *testing.T) {}
-func Test_structTypeToField(t *testing.T) {}
-func Test_chanTypeToField(t *testing.T) {}
+func Test_arrayTypeToField(t *testing.T) {
+	assert.Equal(
+		t,
+		arrayTypeToField(
+			"test",
+			&ast.ArrayType{
+				Elt: &ast.Ident{Name: "string"},
+			},
+		),
+		domain.Field{
+			Name: "test",
+			Type: "[]string",
+		},
+	)
+}
+func Test_ellipsisToField(t *testing.T) {
+	assert.Equal(
+		t,
+		ellipsisToField(
+			"test",
+			&ast.Ellipsis{
+				Elt: &ast.Ident{Name: "string"},
+			},
+		),
+		domain.Field{
+			Name: "test",
+			Type: "... string",
+		},
+	)
+}
+func Test_interfaceTypeToField(t *testing.T) {
+	assert.Equal(
+		t,
+		interfaceTypeToField(
+			"test",
+			&ast.InterfaceType{},
+		),
+		domain.Field{
+			Name: "test",
+			Type: "interface",
+		},
+	)
+}
+func Test_mapTypeToField(t *testing.T) {
+	assert.Equal(
+		t,
+		mapTypeToField(
+			"test",
+			&ast.MapType{
+				Key: &ast.Ident{
+					Name: "string",
+				},
+				Value: &ast.Ident{
+					Name: "int64",
+				},
+			},
+		),
+		domain.Field{
+			Name: "test",
+			Type: "map[string]int64",
+		},
+	)
+}
+func Test_funcTypeToField(t *testing.T) {
+	params := []*ast.Field{
+		{
+			Names: []*ast.Ident{{Name: "bla"}},
+			Type:  &ast.Ident{Name: "string"},
+		},
+		{
+			Names: []*ast.Ident{{Name: "anotherBla"}},
+			Type:  &ast.Ident{Name: "int"},
+		},
+	}
+
+	returns := []*ast.Field{
+		{
+			Names: []*ast.Ident{{Name: "bla"}},
+			Type:  &ast.Ident{Name: "string"},
+		},
+	}
+	assert.Equal(
+		t,
+		funcTypeToField(
+			"test",
+			&ast.FuncType{
+				Params:  &ast.FieldList{List: params},
+				Results: &ast.FieldList{List: returns},
+			},
+		),
+		domain.Field{
+			Name: "test",
+			Type: "func(bla string, anotherBla int) string",
+		},
+	)
+}
+func Test_structTypeToField(t *testing.T) {
+	assert.Equal(
+		t,
+		structTypeToField(
+			"test",
+			&ast.StructType{},
+		),
+		domain.Field{
+			Name: "test",
+			Type: "interface{}",
+		},
+	)
+}
+func Test_chanTypeToField(t *testing.T) {
+	assert.Equal(
+		t,
+		chanTypeToField(
+			"test",
+			&ast.ChanType{
+				Value: &ast.Ident{
+					Name: "string",
+				},
+			},
+		),
+		domain.Field{
+			Name: "test",
+			Type: "chan string",
+		},
+	)
+}
